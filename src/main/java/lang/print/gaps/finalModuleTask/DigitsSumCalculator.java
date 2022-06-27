51301c0e91e3c441ed5e75df22261f0777b61540
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2022-06-27 15:40:13 +0500

    fixed

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int summ = (number % 10) + ((number / 10) % 10) + ((number / 100) % 10) + (number / 1000);
+        System.out.println(summ);
     }
 }
